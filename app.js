70ce11797cada9949cd9f3f0356156f0ef9dadc4
John Shaff <johnshaff@gmail.com>
2016-05-28 16:59:43 -0700

    started to sort object array

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 objectList = [];
 
-function Location (styledname, name, vibe, start, end) {
+function Location (styledname, name, vibe, start, end, url) {
   this.styledname = styledname;
   this.name = name;
-  this.stared = 0;
-  this.end = 0;
+  this.start = start;
+  this.end = end;
   this.vibe = vibe;
   this.imgPath = 'img/' + this.name + '.png';
+  this.url = url;
   this.tallyShown = 0;
   this.tallyClick = 0;
   objectList.push(this);
 };
 
-var stjohns = new Location ('St. Johns', 'stjohns', 'groovy', 5, 8);
+var stjohns = new Location ('St. Johns', 'stjohns', 'groovy', 5, 8, 'http://www.saintjohnsseattle.com/');
 
-console.log(objectList);
+var larrysbar = new Location ('Larry\'s Bar', 'larrysbar', 'dive', 2, 4);
 
-function displayFirstSection () {
+var moesbar = new Location ('Moe\'s Bar', 'moesbar', 'dive', 3, 5);
+
+var curlysbar = new Location ('Curly\'s Bar', 'curlysbar', 'dive', 4, 5);
+
+console.dir(objectList.sort(function (a, b) {
+  return a.start > b.start;
+}));
+
+//Grab the currentTime
+//if current object has start time < current time keep going
+//then push object to openingSoon Array
+
+//if current object has start time > current time
+//then push object to closingSoon Array
+
+
+
+function buildFirstRow () {
   var firstFiver = document.getElementById('first_fiver');
   var newLoc = document.createElement('div');
   newLoc.id = objectList.length - 1;
@@ -24,14 +42,14 @@ function displayFirstSection () {
 
   var createH3 = function () {
     var h3El = document.createElement('h3');
-    h3El.textContent = objectList[0].styledname;
+    h3El.textContent = objectList[0].styledname;//Here
     newLoc.appendChild(h3El);
   };
   createH3();
 
   var createP = function () {
     var pEl = document.createElement('p');
-    pEl.textContent = objectList[0].vibe;
+    pEl.textContent = objectList[0].vibe;//Here
     newLoc.appendChild(pEl);
   };
   createP();
@@ -48,7 +66,10 @@ function displayFirstSection () {
   }
   createClock();
 };
-displayFirstSection();
+buildFirstRow();
+
+
+//I want to populate each row with the
 
 // function that created a clock based on an instance property
 // put that clock on a div
