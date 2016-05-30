8b89bc8afb585d2587ca3567608f9547e7da929b
dylanjsa90 <dylanjsanders1@gmail.com>
2016-05-29 23:35:42 -0700

    compiled the different branches and did some edits to app.js and additions to index.html, clock.js can be deleted

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-objectList = [];
-objectListWeekend = [];
-openingSoon = [];
-closingSoon = [];
-options = [5, 10, 15, 20];
-expandCount = 0;
-resultsSection = document.getElementById("results")
+var objectList = [];
+var objectListWeekend = [];
+var openingSoon = [];
+var closingSoon = [];
+var options = [5, 10, 15, 20];
+var expandCount = 0;
+var results = document.getElementById('results');
 //I can't just go through all instances and build to the index dynamically because we want to show the user their locations after they've been sorted. openingSoon and closingSoon allows us to sort the locations prior to display.
 
-// var today = new Date();
-var today = 4;
+var today = new Date();
+var currentHour = (new Date).getHours();
+var currentMinute = (new Date).getMinutes();
 
 // Testing button for filters... will move soon
-var bodyElement = document.getElementById('body');
-var diveButton = document.createElement('button');
-diveButton.id = 'diveButton';
-diveButton.textContent = 'DIVE FILTER';
-bodyElement.appendChild(diveButton);
-diveButton.addEventListener('click', diveFilter);
-
-
+// var bodyElement = document.getElementById('body');
+// var diveButton = document.createElement('button');
+// diveButton.id = 'diveButton';
+// diveButton.textContent = 'DIVE FILTER';
+// bodyElement.appendChild(diveButton);
+// diveButton.addEventListener('click', diveFilter);
 // if (today.getDay > 5) {
 //   console.log('gotta do the weekend list yo');
 // }
@@ -41,21 +40,6 @@ function Location (styledname, name, vibe, food, start, end, weekend, url) {
   }
 };
 
-var stjohns = new Location ('St. Johns', 'stjohns', 'Groovy', true, 5, 8, 'http://www.saintjohnsseattle.com/');
-var larrysbar = new Location ('Larry\'s Bar', 'larrysbar', 'Groovy', true, 6, 9);
-var moesbar = new Location ('Moe\'s Bar', 'moesbar', 'Uppity', false, 5, 7);
-var curlysbar = new Location ('Curly\'s Bar', 'curlysbar', 'Uppity', false, 4, 7, true);
-var benstavern = new Location ('Ben\'s Tavern', 'benstavern', 'Uppity', false, 5, 8);
-var jerrystavern = new Location ('Jerry\'s Tavern', 'jerrystavern', 'Hipster', true, 6, 8);
-var conans = new Location ('Conan\'s', 'conans', 'Hipster', true, 6, 9, true);
-var johns = new Location ('John\'s', 'johns', 'Hipster', false, 7, 9);
-var bearkats = new Location ('Bearkat\'s', 'jerrystavern', 'Hipster', true, 7, 9);
-var bishops = new Location ('Bishop\'s', 'conans', 'Hipster', true, 7, 9, true);
-var toms = new Location ('Tom\'s', 'toms', 'Dive', true, 8, 9, true);
-var dicks = new Location ('Dick\'s', 'dicks', 'Dive', false, 8, 9);
-var harrys = new Location ('harry\'s', 'harrys', 'Dive', true, 8, 9);
-var yomommas = new Location ('YoMommas\'s', 'yomommas', 'Dive', true, 8, 9, true);
-
 //Creates two arrays for locations opening soon and closing soon
 // for (var i = 0; i < objectList.length; i++)
 //   if (today.getHours() < objectList[i].end) { //Is happy hour over?
@@ -66,163 +50,151 @@ var yomommas = new Location ('YoMommas\'s', 'yomommas', 'Dive', true, 8, 9, true
 //     }
 //   }
 
-//***Testing loop*** falsely creates openingSoon array, delete when done
 //Creates opening and closing arrays
 function buildOpenCloseArrays () {
   console.log('builing opening and closing array');
   for (var i = 0; i < objectList.length; i++) {
-    if (today < objectList[i].end) { //Is happy hour over?
-      if (today > objectList[i].start) {//Has happy hour begun?
-          closingSoon.push(objectList[i]);
+    if (currentHour < objectList[i].end) { //Is happy hour over?
+      if (currentHour >= objectList[i].start) {//Has happy hour begun?
+        closingSoon.push(objectList[i]);
       }
-      if (today < objectList[i].start) {
+      if (currentHour < objectList[i].start) {
         openingSoon.push(objectList[i]);
       }
     }
   }
   console.log('There are ' + (openingSoon.length + closingSoon.length) + ' available Happy Hours');
 }
-buildOpenCloseArrays();
 
 //Sorts all the arrays
-var sortObjectList = function () {
+function sortObjectList () {
   objectList.sort(function (a, b) {
     return a.start > b.start;
   });
 }
-sortObjectList();
 
-var sortClosingSoon = function () {
+function sortClosingSoon() {
   console.log('sorting closing array');
   closingSoon.sort(function (a, b) {
     return a.end > b.end;
   });
 }
-sortClosingSoon();
 
-var sortOpeningSoon = function () {
+function sortOpeningSoon() {
   console.log('sorting opening array');
   openingSoon.sort(function (a, b) {
     return a.start > b.start;
   });
 }
+
+function buildResultsHeader() {
+  var thEl = document.createElement('th');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'Venue name';
+  thEl.appendChild(tdEl);
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Vibe';
+  thEl.appendChild(tdEl);
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Happy Hour Start Time';
+  thEl.appendChild(tdEl);
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Happy Hour End Time';
+  thEl.appendChild(tdEl);
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Happy Hour ends in... ';
+  thEl.appendChild(tdEl);
+  results.appendChild(thEl);
+}
+
+//objectList to be replaced with proper array once we've discussed things
+// for loop also should be adjusted for expand after we discuss that as well
+function buildResults() {
+  var trEl = document.createElement('tr');
+  for (var i = 0; i < 5; i++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = objectList[i].name;
+    trEl.appendChild(tdEl);
+    tdEl = document.createElement('td');
+    tdEl.textContent = objectList[i].vibe;
+    trEl.appendChild(tdEl);
+    tdEl = document.createElement('td');
+    tdEl.textContent = (parseInt(objectList[i].start) - 12) + ':00pm';
+    trEl.appendChild(tdEl);
+    tdEl = document.createElement('td');
+    tdEl.textContent = (parseInt(objectList[i].end) - 12) + ':00pm';
+    trEl.appendChild(tdEl);
+    tdEl = document.createElement('td');
+    // If/else statement to calculate time remaining by subtracting currentHour/instance.start||end
+    // and currentMinute from instance.end and concatentating the string for table output too lazy atm
+    if (currentHour > objectList[i].start) {
+//
+    }
+    trEl.appendChild(tdEl);
+  }
+  results.appendChild(trEl);
+}
+
+// Need to adjust after discussing object arguments and filters, maybe need to add more input fields to html file
+function addNewLocation(event) {
+  var name = event.target.name.value;
+  var start = parseInt(event.target.start.value);
+  var end = parseInt(event.target.end.value);
+  var vibe = event.target.vibe.value;
+  var food = event.target.food.value;
+  var bool;
+  if (food === 'true') {
+    bool = true;
+  } else {
+    bool = false;
+  }
+  var add = new Location(name, vibe, bool, start, end);
+  var lsAdd = JSON.stringify(objectList);
+  localStorage.setItem('locationsArray', lsAdd);
+}
+
+// function removeFood(event) {
+//   for ()
+// }
+
+// Might need to change the for loop to set each property if those don't seem to be set properly
+if (localStorage.getItem('locationsArray') !== null) {
+  var list = localStorage.getItem('locationsArray');
+  list = JSON.parse(list);
+  for (var i = 0; i < list.length; i++) {
+    objectList[i] = list[i];
+  }
+} else {
+  var stjohns = new Location ('St. Johns', 'stjohns', 'Groovy', true, 17, 20, 'http://www.saintjohnsseattle.com/');
+  var larrysbar = new Location ('Larry\'s Bar', 'larrysbar', 'Groovy', true, 18, 20);
+  var moesbar = new Location ('Moe\'s Bar', 'moesbar', 'Uppity', false, 17, 19);
+  var curlysbar = new Location ('Curly\'s Bar', 'curlysbar', 'Uppity', false, 16, 19, true);
+  var benstavern = new Location ('Ben\'s Tavern', 'benstavern', 'Uppity', false, 17, 20);
+  var jerrystavern = new Location ('Jerry\'s Tavern', 'jerrystavern', 'Hipster', true, 18, 20);
+  var conans = new Location ('Conan\'s', 'conans', 'Hipster', true, 18, 21, true);
+  var johns = new Location ('John\'s', 'johns', 'Hipster', false, 19, 21);
+  var bearkats = new Location ('Bearkat\'s', 'jerrystavern', 'Hipster', true, 19, 21);
+  var bishops = new Location ('Bishop\'s', 'conans', 'Hipster', true, 19, 21, true);
+  var toms = new Location ('Tom\'s', 'toms', 'Dive', true, 20, 21, true);
+  var dicks = new Location ('Dick\'s', 'dicks', 'Dive', false, 20, 21);
+  var harrys = new Location ('harry\'s', 'harrys', 'Dive', true, 20, 21);
+  var yomommas = new Location ('YoMommas\'s', 'yomommas', 'Dive', true, 20, 21, true);
+}
+
+buildOpenCloseArrays();
+sortObjectList();
+sortClosingSoon();
 sortOpeningSoon();
 
+buildResultsHeader();
+buildResults();
 
-//openingSoon Row Builder
-var buildOpeningSoonRow = function (i) {
-  var newLoc = document.createElement('div');
-  newLoc.id = 'loc' + openingSoon.indexOf(openingSoon[i]);
-  resultsSection.appendChild(newLoc);
+document.getElementById('form').addEventListener('submit', addNewLocation);
 
-  var createH3 = function () {
-    var h3El = document.createElement('h3');
-    h3El.textContent = openingSoon[i].styledname;//Add location name
-    newLoc.appendChild(h3El);
-  };
-  createH3();
-
-  var createP = function () {
-    var pEl = document.createElement('p');
-    pEl.textContent = openingSoon[i].vibe;//Add location vibe
-    newLoc.appendChild(pEl);
-  };
-  createP();
-
-  // var createClock = function () {
-  //   var setTimer = function (){
-  //     //something that pulls in props from an instance and creates a timer
-  //     var newTimer = 'Countdown Timer';
-  //     var newClock = document.createElement('div');
-  //     newClock.textContent = newTimer; //Add location countdown
-  //     newLoc.appendChild(newClock);
-  //   }
-  //   setTimer();
-  // }
-  // createClock();
-};
-
-//closingSoon Row Builder
-var buildClosingSoonRow = function (i) {
-  var newLoc = document.createElement('div');
-  newLoc.id = 'loc' + closingSoon.indexOf(closingSoon[i]);//Prob doesn't need to be numbered
-  resultsSection.appendChild(newLoc);
-
-  var createH3 = function () {
-    var h3El = document.createElement('h3');
-    h3El.textContent = closingSoon[i].styledname;//Add location name
-    newLoc.appendChild(h3El);
-  };
-  createH3();
-
-  var createP = function () {
-    var pEl = document.createElement('p');
-    pEl.textContent = closingSoon[i].vibe;//Add location vibe
-    newLoc.appendChild(pEl);
-  };
-  createP();
-
-  var createClock = function () {
-    var setTimer = function (){
-      //something that pulls in props from an instance and creates a timer
-      var newTimer = 'Countdown Timer';
-      var newClock = document.createElement('div');
-      newClock.textContent = newTimer; //Add location countdown
-      newLoc.appendChild(newClock);
-      newClock.style.color = "#cc0000";
-    }
-    setTimer();
-  }
-  createClock();
-};
-
-//Builds first five taking first from openingSoon and then from closingSoon
-var sectionBuild = function (numResults) {
-  for (var i = 0; i < openingSoon.length; i++) {
-    if (resultsSection.childElementCount < options[numResults]) {
-      console.log('building openingSoon row with i @ ' + i);
-      buildOpeningSoonRow(i); //very interesting that i doesn't automatically scope down into this function.
-    }
-  }
-
-  for (var i = 0; i < closingSoon.length; i++) {
-    console.log('building closingSoon row with i @ ' + i);
-    if (resultsSection.childElementCount < options[numResults]) {
-      buildClosingSoonRow(i);
-    }
-  }
-}
-sectionBuild(0);
-
-//Creates button to expand results based on the amount you want shown
-function expander (numResults) {
-    if (openingSoon.length + closingSoon.length > options[numResults]) {
-    var expandButton = document.createElement('button');
-    expandButton.id = 'expandButton';
-    expandButton.textContent = 'See More';
-    var expandDiv = document.getElementById('expandDiv');
-    expandDiv.appendChild(expandButton);
-    ++expandCount;
-    expandButton.addEventListener('click', expandList);
-  }
-}
-expander(expandCount);
-
-
-function expandList (event) { //This happens when there's more options
-  expandDiv.removeChild(expandButton); //Removes button
-  while (resultsSection.firstChild) { //While the resultsSection has a first child
-    resultsSection.removeChild(resultsSection.firstChild);//Remove all the children
-  }
-  sectionBuild(expandCount);//Build the section again now that expandCount has been plused up
-  expander(expandCount);//Show the button if there's still more
-}
-
-function diveFilter (event) {
-
-}
-
-
+// function diveFilter (event) {
+//
+// }
 
 //if they hit the vibe button, I want to resort my array by vibe then display
 //if they hit the food button, I want to remove any locations without food
+document.getElementById('food').addEventListener('click', removeFood);
--- clock.js
+++ clock.js
@@ -19,26 +19,26 @@ function getTimeRemaining (endtime) {
 
 // hold the remaining time until the deadline. The Date.parse() function is native JavaScript that converts a time string into a value in milliseconds.
 
-var t = Date.parse(endtime) - Date.parse(new Date());
+// var t = Date.parse(endtime) - Date.parse(new Date());
 
 // convert the milliseconds to days, hours, minutes, and seconds.
-var seconds = Math.floor((t / 1000) % 60);
+// var seconds = Math.floor((t / 1000) % 60);
 
 // With the days, hours, minutes, and seconds prepared, we’re now ready to return the data as a reusable object:
-return {
-  'total': t,
-  'days': days,
-  'hours': hours,
-  'minutes': minutes,
-  'seconds': seconds
-};
+// return {
+//   'total': t,
+//   'days': days,
+//   'hours': hours,
+//   'minutes': minutes,
+//   'seconds': seconds
+// };
 
 // This object allows you to call your function and get any of the calculated values. Here’s an example of how you’d get the remaining minutes:
-getTimeRemaining(deadline).minutes
+// getTimeRemaining(deadline).minutes
 
 // Now that we have a function that spits out the days, hours, minutes, and seconds remaining, we can build our clock. First we’ll create the following HTML element to hold our clock:
 
-<div id="clockdiv"></div>
+// <div id="clockdiv"></div>
 
 // Then we’ll write a function that outputs the clock data inside our new div. This function takes two parameters: the id of the element that will contain our clock and the end time of the countdown. Inside the function, we’ll declare a variable called clock and use it to store a reference to our clock container div so that we don’t have to keep querying the DOM.
 
@@ -46,17 +46,12 @@ function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   var timeinterval = setInterval(function(){
     var t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + '<br>' +
-                      'hours: '+ t.hours + '<br>' +
-                      'minutes: ' + t.minutes + '<br>' +
-                      'seconds: ' + t.seconds;
-    if(t.total<=0){
+    clock.innerHTML = 'days: ' + t.days + '<br>' + 'hours: ' + t.hours + '<br>' + 'minutes: ' + t.minutes + '<br>' + 'seconds: ' + t.seconds;
+    if(t.total <= 0){
       clearInterval(timeinterval);
     }
   },1000);
 }
 
 // At this point, the only remaining step is to run the clock like so:
-
-
 initializeClock('clockdiv', deadline);
