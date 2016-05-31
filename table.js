420cf9e6ad1eac1b96b46016245dd4b4d631dcde
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-05-31 16:12:02 -0700

    got the sorting to work

--- table.js
+++ table.js
@@ -5,12 +5,15 @@ var closingSoon = [];
 var options = [5, 10, 15, 20];
 var expandCount = 0;
 
+var relaxingFilter = false;
 var relaxingOpeningSoon = [];
 var relaxingClosingSoon = [];
 
+var upbeatFilter = false;
 var upbeatOpeningSoon = [];
 var upbeatClosingClosing = [];
 
+var refinedFilter = false;
 var refinedOpeningSoon = [];
 var refinedClosingSoon = [];
 
@@ -22,11 +25,15 @@ var today = 4;
 
 // Testing button for filters... will move soon
 var bodyElement = document.getElementById('body');
-var diveButton = document.createElement('button');
-diveButton.id = 'diveButton';
-diveButton.textContent = 'DIVE FILTER';
-bodyElement.appendChild(diveButton);
-diveButton.addEventListener('click', diveFilter);
+
+var refinedFilter = document.getElementById('vibe');
+refinedFilter.addEventListener('click', refinedFilterHandler);
+
+var foodFilter = document.getElementById('food');
+foodFilter.addEventListener('click', foodFilter);
+
+var timeFilter = document.getElementById('time');
+timeFilter.addEventListener('click', timeFilter);
 
 // if (today.getDay > 5) {
 //   console.log('gotta do the weekend list yo');
@@ -48,22 +55,26 @@ function Location (styledname, name, vibe, food, start, end, weekend, url) {
   if (weekend === true) {
     objectListWeekend.push(this);
   };
+  // if ('Refined' === this.vibe) {
+  //   refinedClosingSoon.push(objectList[i]);
+  // }
 };
 
-var stjohns = new Location ('St. Johns', 'stjohns', 'Groovy', true, 5, 8, 'http://www.saintjohnsseattle.com/');
-var larrysbar = new Location ('Larry\'s Bar', 'larrysbar', 'Groovy', true, 6, 9);
-var moesbar = new Location ('Moe\'s Bar', 'moesbar', 'Uppity', false, 5, 7);
-var curlysbar = new Location ('Curly\'s Bar', 'curlysbar', 'Uppity', false, 4, 7, true);
-var benstavern = new Location ('Ben\'s Tavern', 'benstavern', 'Uppity', false, 5, 8);
-var jerrystavern = new Location ('Jerry\'s Tavern', 'jerrystavern', 'Hipster', true, 6, 8);
-var conans = new Location ('Conan\'s', 'conans', 'Hipster', true, 6, 9, true);
-var johns = new Location ('John\'s', 'johns', 'Hipster', false, 7, 9);
-var bearkats = new Location ('Bearkat\'s', 'jerrystavern', 'Hipster', true, 7, 9);
-var bishops = new Location ('Bishop\'s', 'conans', 'Hipster', true, 7, 9, true);
-var toms = new Location ('Tom\'s', 'toms', 'Dive', true, 8, 9, true);
-var dicks = new Location ('Dick\'s', 'dicks', 'Dive', false, 8, 9);
-var harrys = new Location ('Harry\'s', 'harrys', 'Dive', true, 8, 9);
-var yomommas = new Location ('YoMommas\'s', 'yomommas', 'Dive', true, 8, 9, true);
+var stjohns = new Location ('St. Johns', 'stjohns', 'Refined', true, 5, 8, 'http://www.saintjohnsseattle.com/');
+var larrysbar = new Location ('Larry\'s Bar', 'larrysbar', 'Refined', true, 6, 9);
+var moesbar = new Location ('Moe\'s Bar', 'moesbar', 'Refined', false, 5, 7);
+var curlysbar = new Location ('Curly\'s Bar', 'curlysbar', 'Refined', false, 4, 7, true);
+var benstavern = new Location ('Ben\'s Tavern', 'benstavern', 'Refined', false, 5, 8);
+var jerrystavern = new Location ('Jerry\'s Tavern', 'jerrystavern', 'Upbeat', true, 6, 8);
+var conans = new Location ('Conan\'s', 'conans', 'Upbeat', true, 6, 9, true);
+var johns = new Location ('John\'s', 'johns', 'Upbeat', false, 7, 9);
+var bearkats = new Location ('Bearkat\'s', 'jerrystavern', 'Upbeat', true, 7, 9);
+var bishops = new Location ('Bishop\'s', 'conans', 'Upbeat', true, 7, 9, true);
+var toms = new Location ('Tom\'s', 'toms', 'Relaxing', true, 8, 9, true);
+var dicks = new Location ('Dick\'s', 'dicks', 'Relaxing', false, 8, 9);
+var harrys = new Location ('Harry\'s', 'harrys', 'Relaxing', true, 8, 9);
+var yomommas = new Location ('YoMommas\'s', 'yomommas', 'Relaxing', true, 8, 9, true);
+
 
 //Creates two arrays for locations opening soon and closing soon
 // for (var i = 0; i < objectList.length; i++)
@@ -83,31 +94,31 @@ function buildOpenCloseArrays () {
     if (today < objectList[i].end) { //Is happy hour over?
       if (today > objectList[i].start) {//Has happy hour begun?
         closingSoon.push(objectList[i]);
-        if ('refined' === objectList[i].vibe) {
+        if ('Refined' === objectList[i].vibe) {
           refinedClosingSoon.push(objectList[i]);
         }
-        if ('relaxing' === objectList[i].vibe) {
+        if ('Relaxing' === objectList[i].vibe) {
           relaxingClosingSoon.push(objectList[i]);
         }
-        if ('upbeat' === objectList[i].vibe) {
+        if ('Upbeat' === objectList[i].vibe) {
           upbeatClosingClosing.push(objectList[i]);
         }
       }
       if (today < objectList[i].start) {
         openingSoon.push(objectList[i]);
-        if ('refined' === objectList[i].vibe) {
+        if ('Refined' === objectList[i].vibe) {
           refinedOpeningSoon.push(objectList[i]);
         }
-        if ('relaxing' === objectList[i].vibe) {
-          refinedOpeningSoon.push(objectList[i]);
+        if ('Relaxing' === objectList[i].vibe) {
+          relaxingOpeningSoon.push(objectList[i]);
         }
-        if ('upbeat' === objectList[i].vibe) {
-          refinedOpeningSoon.push(objectList[i]);
+        if ('Upbeat' === objectList[i].vibe) {
+          upbeatOpeningSoon.push(objectList[i]);
         }
       }
     }
   }
-  console.log('There are ' + (openingSoon.length + closingSoon.length) + ' available Happy Hours');
+  console.log('There are ' + (openingSoon.length + closingSoon.length) + ' available Happy Hours. ' + relaxingOpeningSoon.length + ' are relaxing Happy Hours opening soon. ' + relaxingClosingSoon.length + ' are relaxing Happy Hours closing soon. ' + refinedOpeningSoon.length + ' are refined Happy Hours opening soon. ' + refinedClosingSoon.length + ' are refined Happy Hours closing soon. ' + upbeatOpeningSoon.length + ' are upbeat Happy Hours opening soon. ' + upbeatClosingClosing.length + ' are upbeat Happy Hours closing soon.');
 }
 buildOpenCloseArrays();
 
@@ -152,7 +163,6 @@ var sortAllClosingSoon = function () {
   });
 };
 sortAllClosingSoon();
-
 
 //openingSoon Row Builder
 var buildOpeningSoonRow = function (i, arrayUsed) {
@@ -238,8 +248,9 @@ var sectionBuild = function (numResults, openingSoonArray, closingSoonArray) {
 sectionBuild(0, openingSoon, closingSoon);
 
 //Creates button to expand results based on the amount you want shown
-function expander (numResults) {
-  if (openingSoon.length + closingSoon.length > options[numResults]) {
+function expander (numResults, expandingFromOpeningArray, expandingFromClosingArray) {
+  console.log(expandingFromOpeningArray.length);
+  if (expandingFromOpeningArray.length + expandingFromClosingArray.length > options[numResults]) {
     var expandButton = document.createElement('button');
     expandButton.id = 'expandButton';
     expandButton.textContent = 'See More';
@@ -249,7 +260,7 @@ function expander (numResults) {
     expandButton.addEventListener('click', expandList);
   }
 }
-expander(expandCount);
+expander(expandCount, openingSoon, closingSoon);
 
 function expandList (event) { //This happens when there's more options
   expandDiv.removeChild(expandButton); //Removes button
@@ -257,79 +268,19 @@ function expandList (event) { //This happens when there's more options
     resultsTable.removeChild(resultsTable.firstChild);//Remove all the children
   }
   sectionBuild(expandCount, openingSoon, closingSoon);//Build the section again now that expandCount has been plused up
-  expander(expandCount);//Show the button if there's still more
+  expander(expandCount, openingSoon, closingSoon);//Show the button if there's still more
 }
 
-//There doesn't seem to be any way to store things from the DOM in order to remove them
-//You can't push the DOM elements to an array because they don't show up as removable elements
-//You can't store an element in a variable and then remove the element stored in the variable from DOM
-//You can't even iterate over the DOM because you can't test the innerHTML text against a string
-//No matter where I define the i, or whether I use a while or for loop, the i looses its scope
-//This crazy nonsense is the a work in progress
-
-// function diveFilter (event) {
-//   console.log('in');
-//   var i = 1;
-//   while (i < resultsTable.childElementCount + 1) {
-//     console.log(i);
-//     var checkMe = resultsTable.childNodes[i].childNodes[1].innerHTML;
-//     i++;
-//     if (checkMe === 'Hipster') {
-//       console.log(checkMe === 'Hipster');
-//       console.log(resultsTable.childNodes[i - 1]);
-//       resultsTable.removeChild(resultsTable.childNodes[i - 1]);
-//     }
-//   }
-// }
-
-// Even with methods available to table elements, once you try to access the DOM from within a For loop everything breaks down.
-// function diveFilter (event) {
-//   var i = 1;
-//   console.log(typeof(resultsTable.childNodes[i].childNodes[i].innerHTML));
-//   var store = resultsTable.childNodes[i].childNodes[i].innerHTML;
-//   console.log(store === 'Hipster');
-//   for (var i = 0; i < resultsTable.childElementCount; i++) {
-//     var store = resultsTable.childNodes[i].childNodes[i].innerHTML;
-//     if (store === 'Hipster') {
-//       resultsTable.deleteRow(resultsTable.childNodes[i]);
-//       console.log('Killed it with fire');
-//     }
-//   }
-// }
-
-function diveFilter (event) {
-  // var i = 1;
-  // console.log(typeof(resultsTable.childNodes[i].childNodes[i].innerHTML));
-  // var store = resultsTable.childNodes[i].childNodes[i].innerHTML;
-  // console.log(store === 'Hipster');
-  // for (var i = 0; i < resultsTable.childElementCount; i++) {
-  //   var store = resultsTable.childNodes[i].childNodes[i].innerHTML;
-  //   if (store === 'Hipster') {
-  //     resultsTable.deleteRow(resultsTable.childNodes[i]);
-  //     console.log('Killed it with fire');
-  //   }
-  // }
-  //expandCount = 0;
+function refinedFilterHandler (event) {
+  console.log('Vibin bro');
+  expandCount = 0;
+  while (resultsTable.firstChild) { //While the resultsTable has a first child
+    resultsTable.removeChild(resultsTable.firstChild);//Remove all the children
+  }
+  sectionBuild(expandCount, refinedOpeningSoon, refinedClosingSoon);//Build the section again now that expandCount has been plused up
+  expander(expandCount, refinedOpeningSoon, refinedClosingSoon);//Show the button if there's still more
+  ++expandCount;
 }
-
-// var buildRelaxingOpeningSoonRow = function (i) {
-//   var newLoc = document.createElement('tr');
-//   newLoc.id = 'loc' + openingSoon.indexOf(openingSoon[i]);
-//   resultsTable.appendChild(newLoc);
-//
-//   var createTdName = function () {
-//     var tdEl = document.createElement('td');
-//     tdEl.textContent = openingSoon[i].styledname;//Add location name
-//     newLoc.appendChild(tdEl);
-//   };
-//   createTdName();
-//
-//   var createVibe = function () {
-//     var tdEl = document.createElement('td');
-//     tdEl.textContent = openingSoon[i].vibe;//Add location vibe
-//     newLoc.appendChild(tdEl);
-//   };
-//   createVibe();
 
 //if they hit the vibe button, I want to resort my array by vibe then display
 //if they hit the food button, I want to remove any locations without food
