ef3ec75e0d2c7c8b104dcf5afaf1fa26830d977c
Michael Bishop <polishthebishop@gmail.com>
2016-06-01 15:50:24 -0700

    Finish Question Section

--- app-questions.js
+++ app-questions.js
@@ -2,12 +2,12 @@ var allQuestions = ['Are you looking for a bar with more of a relaxed feeling?',
 var yesButton = document.getElementById('yes-button');
 var noButton = document.getElementById('no-button');
 var nextButton = document.getElementById('next-button');
+var questionSection = document.getElementById('question-section');
 var indexCounter = 0;
-
+var maxQuestions = 4;
 
 var questionArea = document.getElementById('question-id');
 var answerArea = document.getElementById('answer-id');
-
 
 function Questions (location, question, answer) {
   this.location = location;
@@ -17,22 +17,42 @@ function Questions (location, question, answer) {
 }
 var twoBells = new Questions('Two Bells', 'Want a friendly bar?', 'Try Two Bells');
 var bathTubGin = new Questions('Bathtub Gin', 'Want a Speak Easy?', 'Bathtub Gin is your choice');
-var theWhiskeyBar = new Questions('The Whiskey Bar', 'Would you like the ultimate whiskey seletion?', 'You\'re only option is The Whiskey Bar');
+var theWhiskeyBar = new Questions('The Whiskey Bar', 'Would you like the ultimate whiskey selection?', 'You\'re only option is The Whiskey Bar');
 var amber = new Questions('Amber', 'Want more of a classy environment with dancing as an option?', 'Amber is the environment for you');
 
-
 var allBars = [twoBells, bathTubGin, theWhiskeyBar, amber];
-questionArea.innerText = 'Question: ' + allQuestions[0];
-answerArea.innerText = 'Answer: ' ;
+// questionArea.innerText = 'H2 Question Area';
+questionArea.innerText = 'Question: ' + allBars[indexCounter].question;
 
 // if the index number equals length of array, it must equal to 0
 
 function nextQuestion() {
   //  when user hits the next button, the next question is displayed into the questionArea
-  questionArea.innerText = allBars[indexCounter].question;
   indexCounter += 1;
+  if (indexCounter < maxQuestions) {
+    answerArea.innerText = '';
+    questionArea.innerText = 'Question: ' + allBars[indexCounter].question;
+    // indexCounter = 0;
+  } else {
+    // alert('You have gone through all the questions');
+    answerArea.innerText = 'Check the table for other selections';
+    nextButton.hidden = true;
+    yesButton.hidden = true;
+    noButton.hidden = true;
+    questionArea.innerText = '';
+
+  }
+
+}
+function clickYes() {
+  // when user hits the yes button, the answer will appear below
+  answerArea.innerText = 'Answer: ' + allBars[indexCounter].answer;
+}
+function clickNo() {
+  // when user clicks the no button, answer will display a nice message
+  answerArea.innerText = 'Click the Next button and see if we can\'t find you something better';
 }
 
-if (indexCounter >= 4)
-
 nextButton.addEventListener('click', nextQuestion);
+yesButton.addEventListener('click', clickYes);
+noButton.addEventListener('click', clickNo);
